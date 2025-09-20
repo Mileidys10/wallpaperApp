5820b7a7ffaeea33884c9ec25534496e7ca6a1b7
Mileidys Agamez <agamezmileidys@gmail.com>
2025-09-20 18:00:08 -0500

    auth hecho

--- src/app/provide/auth/auth.ts
+++ src/app/provide/auth/auth.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Auth as AuthFirebase, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Auth {
+
+  constructor(
+    private readonly afb: AuthFirebase,
+  ) {}
+
+  async register(email: string, password: string) {
+    try {
+      await createUserWithEmailAndPassword(
+        this.afb,
+        email,
+        password,
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async logIn(email: string, password: string) {
+    try {
+      await signInWithEmailAndPassword(
+        this.afb,
+    email,
+        password,
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async logOut() {
+    try {
+      await signOut(this.afb);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   
 }
